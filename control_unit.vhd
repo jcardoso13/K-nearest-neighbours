9acb4739e876f65eebce8a1676d133855cd6167d
micasserodio <micaelaserodio08@gmail.com>
2018-11-30 18:26:35 +0000

    circuit.vhd, adder.vhd and control_unit.vhd ready to be changed

--- control_unit.vhd
+++ control_unit.vhd
@@ -7,19 +7,15 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity control_unit is
 
 port(
+	--input--
 	clk: in std_logic;
 	rst: in std_logic;
-	sel_reg1,sel_reg2,sel_reg3,sel_reg4:out std_logic_vector(2 downto 0);
-	sel_reg5,sel_reg6: out std_logic_vector(1 downto 0); 
-	sel_out1,sel_out2,sel_out3,
-	sel_out4: out std_logic_vector(1 downto 0);
-	load: out std_logic_vector(3 downto 0);
-	sel_add: out std_logic_vector(1 downto 0);
-	done: out std_logic;
-	trunc: out std_logic;
-	seq: out std_logic
+	init: in std_logic;
+	k: in std_logic;
 	
-
+	--output--
+	k_out: out std_logic;
+	load: out std_logic
 );
 
 end control_unit;
@@ -27,7 +23,7 @@ end control_unit;
 
 architecture Behavioral of control_unit is
 
-	type fsm_states is ( s_initial, s_cycle1, s_cycle2, s_cycle3, s_cycle4, s_cycle5, s_cycle6,s_end);
+	type fsm_states is ( s_initial, s_execution,s_end);
 	signal currstate, nextstate: fsm_states;
 	
 
@@ -51,150 +47,14 @@ begin  --  process
     
 	case currstate is
 		when s_initial =>
-			nextstate <= s_cycle1;
-	    		load <="0000";
-        		sel_reg1 <= "XXX";
-        		sel_reg2 <= "XXX";
-        		sel_reg3 <= "XXX";
-        		sel_reg4 <= "XXX";
-		        sel_reg5 <= "XX";
-		        sel_reg6 <= "XX";
-        		sel_out1 <= "XX";
-       			sel_out2 <= "XX";
-        		sel_out3 <= "XX";
-        		sel_out4 <= "XX";
-        		sel_add <= "XX";
-        		trunc <= '0';
-        		seq <= '0';
-        		done <= '0';
+			nextstate <= s_execution;
 			
-		when s_cycle1 =>
-			nextstate <= s_cycle2;
-			sel_reg1 <= "110"; --Q10
-			sel_reg2 <= "110"; --Q00
-			sel_reg3 <= "101"; --x
-			sel_reg4 <= "101"; --x0
-			sel_reg5 <= "XX";
-			sel_reg6 <= "XX";
-			sel_out1 <= "00"; --saves the result of adder1 in R1
-			sel_out2 <= "01"; --saves the result of adder2 in R2
-			sel_out3 <= "XX";
-			sel_out4 <= "XX";
-			load <= "0011";  -- enable of R1 and R2
-			sel_add  <= "11"; -- 2 subtractions
-			trunc <= '0'; 
-			seq <= '0';
-			done <= '0';
-		
-		when s_cycle2 =>
-			nextstate <= s_cycle3;
-			sel_reg1 <= "100"; --y
-			sel_reg2 <= "100"; --y0
-			sel_reg3 <= "111"; --Q11
-			sel_reg4 <= "111"; --Q01
-			sel_reg5 <= "00"; --R1
-			sel_reg6 <= "01"; --R2
-			sel_out1 <= "00" ; --saves the result of adder1
-			sel_out2 <= "XX";
-			sel_out3 <= "01" ; --saves the result of adder2
-			sel_out4 <= "10" ; --saves the result of mult
-			load <= "1101"; --enable of R3, R4 and R1
-			sel_add  <= "11"; --subtraction in adder1
-			trunc <= '1'; -- truncate the result of mult 
-			seq <= '0';
-			done <= '0';
-			
-		
-		when s_cycle3 =>
-			nextstate <= s_cycle4;
-			sel_reg1 <= "011"; --R4
-			sel_reg2 <= "110"; --Q00 
-			sel_reg3 <= "XXX"; --not used
-			sel_reg4 <= "XXX"; --not used
-			sel_reg5 <= "10"; --R3
-			sel_reg6 <= "01"; --R2
-			sel_out1 <= "XX";
-			sel_out2 <= "00"; --saves the result of adder1 
-			sel_out3 <= "XX";
-			sel_out4 <= "10"; --saves the result of mult
-			load <= "1010"; 
-			sel_add  <= "X0"; --add in adder1
-			trunc <= '1'; -- truncates the mult result
-			done <= '0';
-			seq <= '0';
-		when s_cycle4 =>
-			nextstate <= s_cycle5;
-			sel_reg1 <= "011"; --R4
-			sel_reg2 <= "111"; --Q01 
-			sel_reg3 <= "010"; -- R2
-			sel_reg4 <= "001"; -- R3
-			sel_reg5 <= "XX"; --not used
-			sel_reg6 <= "XX"; --not used
-			sel_out1 <= "XX";
-			sel_out2 <= "XX";
-			sel_out3 <= "00"; --saves the result of adder1 
-			sel_out4 <= "01"; --saves the result of adder2
-			load <= "1100"; 
-			sel_add  <= "10"; --add in adder1 and sub in adder 2
-			trunc <= '0';
-			seq <= '1';
-			done <= '0';
-		
-		when s_cycle5 =>
-			nextstate <= s_cycle6;	
-			sel_reg1 <= "XXX"; --not used
-			sel_reg2 <= "XXX"; --not used
-			sel_reg3 <= "XXX"; --not used
-			sel_reg4 <= "XXX"; --not used
-			sel_reg5 <= "11"; --R4
-			sel_reg6 <= "00"; --R1
-			sel_out1 <= "XX";
-			sel_out2 <= "XX";
-			sel_out3 <= "10"; --saves the result of the mult
-			sel_out4 <= "XX";
-			load <= "0100"; 
-			sel_add  <= "XX"; 
-			done <= '0';
-			trunc <= '1'; -- truncates the mult result
-			seq <= '0';
-			
-		
-		when s_cycle6 =>
+		when s_execution =>
 			nextstate <= s_end;
-			sel_reg1 <= "010"; --R3
-			sel_reg2 <= "001"; --R2
-			sel_reg3 <= "XXX"; --not used
-			sel_reg4 <= "XXX"; --not used
-			sel_reg5 <= "XX"; --not used
-			sel_reg6 <= "XX"; --not used
-			sel_out1 <= "XX";
-			sel_out2 <= "XX";
-			sel_out3 <= "XX";
-			sel_out4 <= "00"; --saves the result of adder1
-			load <= "1000"; 
-			sel_add  <= "X0"; --add in adder1
-			trunc <= '0';
-			seq <= '0';
-			done <= '0'; 
-
 			
 		when s_end =>
 			nextstate <= s_initial;
-			load <="0000";
-			sel_reg1 <= "XXX";
-			sel_reg2 <= "XXX";
-			sel_reg3 <= "XXX";
-			sel_reg4 <= "XXX";
-			sel_reg5 <= "XX";
-			sel_reg6 <= "XX";
-			sel_out1 <= "XX";
-			sel_out2 <= "XX";
-			sel_out3 <= "XX";
-			sel_out4 <= "XX";
-			sel_add <= "XX";
-			trunc <= '0';
-			seq <= '0';
-			done <= '1';
+			
 	 end case;
  end process;
 end Behavioral;
